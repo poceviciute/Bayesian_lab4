ebc0bbc9bcafda2ccdd1c6303e3a2bd42ba6a9ea
Fanny Karelius <fanny.karelius@telia.com>
2018-05-16 14:25:04 +0200

    final

--- lab4.R
+++ lab4.R
@@ -4,8 +4,8 @@
 
 #a)
 data <- read.table("eBayNumberOfBidderdata.dat", header = TRUE)
-c <- c(1,3:ncol(data))
-data_noC <- data[,c]
+cols <- c(1,3:ncol(data))
+data_noC <- data[,cols]
 glm_fit <- glm(nBids~., family=poisson, data_noC)
 summary(glm_fit)
 #Intercept, VerifyID, Sealed, MajBlem, LogBook, MinBidShare
@@ -50,15 +50,17 @@ metropolis <- function(n, c, sigma, logpostfun,theta,...){
   #logpostfun(theta,...)
   temp1 <-logpostfun(thetas[1,],...)
   #print(thetas[1,])
+  acc_prob <- vector(length=n)
+  acc_prob[1] <- 0
   for(i in 1:n){
     temp_theta<-rmvnorm(n=1, thetas[i,], c*sigma)
     temp2 <-logpostfun(as.vector(temp_theta),...)
     #print(paste("iter",i,"temp2",temp2))
-    acc_prob <- min(1,exp(temp2-temp1))
+    acc_prob[i+1] <- min(1,exp(temp2-temp1))
     #print(acc_prob)
     u <- runif(n = 1, 0,1)
     #print(u)
-    if(u>acc_prob){
+    if(u>acc_prob[i+1]){
       thetas[i+1,]<-thetas[i,]
     }
     else{
@@ -67,9 +69,15 @@ metropolis <- function(n, c, sigma, logpostfun,theta,...){
     }
     
   }
-  thetas
+  data.frame(thetas, "acc.prob"=acc_prob)
 }
-beta_metro <- metropolis(1000, c=1, sigma=inv_hessian, log_poisson, theta=betas_init, y=y, x=x)
+metro2 <- metropolis(1000, c=1, sigma=inv_hessian, log_poisson, theta=betas_init, y=y, x=x)
+metro <- metropolis(1000, c=0.5, sigma=inv_hessian, log_poisson, theta=betas_init, y=y, x=x)
+
+acc_prob <- as.vector(metro$acc.prob)
+avg_acc <- mean(acc_prob)
+
+beta_metro <- as.matrix(metro[,1:length(betas_init)])
 phi <- exp(beta_metro)
 plot(phi[,1], type="l")
 for(i in 1:ncol(phi)){
@@ -78,9 +86,20 @@ for(i in 1:ncol(phi)){
 phi_means <- colMeans(phi)
 
 for(i in 1:ncol(phi)){
-  hist(phi[,i], freq=FALSE, ylab=paste0("phi_", i), main=paste0("phi_", i))
+  hist(phi[,i], freq=FALSE, xlab=paste0("phi_", i), main=paste0("phi_", i))
+  lines(density(phi[,i]), col="red")
 }
 
 
 #d)
 
+x_vec <- c(1,1,1,1,0,0,0,1,0.5)
+lambda <- exp(x_vec%*%t(beta_metro))
+y_mat <- apply(lambda, 1, function(i){rpois(1000, i)})
+hist(y_mat, freq = FALSE, xlab="y_pred")
+lines(density(y_mat))
+hist(lambda)
+prob_y0 <- length(y_mat[y_mat==0])/length(y_mat)
+
+#data[which(data$nBids==0),]
+#data[,x_vec]
